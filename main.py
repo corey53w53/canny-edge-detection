573a4158c40c6493b6ddb19de9e7c02c2e7e2f17
Corey Wang <clw233@cornell.edu>
2023-10-07 14:41:54 -0400

    Finish Task 4 (Co-written by Yunoo and Corey)
    - Implement non-maximum suppression to thin out the edges
    - Upload a non-square test image to make sure they work

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import matplotlib.pyplot as plt
 from PIL import Image, ImageFilter
 
 # task 1
-image = matplotlib.image.imread("flower.jpg")
+image = matplotlib.image.imread("leaf.jpg")
+height = image.shape[0]
+width = image.shape[1]
 intensities = (0.299 * image[:, :, 0]) + \
     (0.587 * image[:, :, 1]) + \
     (0.114 * image[:, :, 2])
@@ -45,13 +47,49 @@ gx_image = blurred_image.filter(ImageFilter.Kernel((3, 3), gx.flatten(), 1))
 gy_image = blurred_image.filter(ImageFilter.Kernel((3, 3), gy.flatten(), 1))
 gx_numpy = np.array(gx_image)
 gy_numpy = np.array(gy_image)
-g_numpy = np.sqrt(gx_numpy.astype(np.uint64) **
-                  2 + gy_numpy.astype(np.uint64)**2)
-theta = np.arctan2(gx_numpy, gy_numpy)
+g_numpy = np.sqrt(gx_numpy[:, :, 0].astype(np.uint64) **
+                  2 + gy_numpy[:, :, 0].astype(np.uint64)**2)
+theta = np.arctan2(gx_numpy[:, :, 0], gy_numpy[:, :, 0])
 plt.imshow(gx_numpy)
 plt.axis("off")
 plt.show()
 
 plt.imshow(gy_numpy)
+plt.axis("off")
+plt.show()
+
+final_gradient = np.maximum(gx_numpy, gy_numpy)
+
+
+# task 4
+
+x_vals = np.array([np.arange(width) for _ in range(height)])
+y_vals = np.array([[i]*width for i in range(height)])
+plt.imshow(final_gradient)
+plt.axis("off")
+plt.show()
+
+
+def nmsuppression(iy, ix):
+    # magnitude of pixel's gradient
+    magnitude = g_numpy[iy][ix]
+    # in y direction
+    if theta[iy][ix] > np.pi/4:
+        # local maximum
+        if 0 < iy and iy+1 < height and (magnitude < g_numpy[iy+1][ix] or magnitude < g_numpy[iy-1][ix]):
+            # suppress by setting to 0
+            final_gradient[iy][ix] = 0
+    # in x direction
+    else:
+        if 0 < ix and ix+1 < width and (magnitude < g_numpy[iy][ix+1] or magnitude < g_numpy[iy][ix-1]):
+            # suppress by setting to 0
+            final_gradient[iy][ix] = 0
+
+
+vnmsuppression = np.vectorize(nmsuppression)
+
+vnmsuppression(y_vals, x_vals)
+
+plt.imshow(final_gradient)
 plt.axis("off")
 plt.show()
