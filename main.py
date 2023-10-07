d6211997ec9247d85c0f0f7b384a856362477168
Corey Wang <clw233@cornell.edu>
2023-10-07 13:42:47 -0400

    Complete Task 3 (Co-written by Yunoo and Corey)
    - Find the image gradients in the x and y direction using the sobel operator
    - Calculate the gradient magnitude and direction

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 from PIL import Image, ImageFilter
 
+# task 1
 image = matplotlib.image.imread("flower.jpg")
 intensities = (0.299 * image[:, :, 0]) + \
     (0.587 * image[:, :, 1]) + \
@@ -18,6 +19,8 @@ plt.imshow(grayscale_image)
 plt.axis("off")
 plt.show()
 
+# task 2
+
 pillow_image = Image.fromarray(grayscale_image.astype(np.uint8))
 gaussian_kernel = np.array([[1, 2, 1],
                             [2, 4, 2],
@@ -26,5 +29,29 @@ blurred_image = pillow_image.filter(
     ImageFilter.Kernel((3, 3), gaussian_kernel.flatten()))
 
 plt.imshow(blurred_image)
+plt.axis("off")
+plt.show()
+
+# task 3
+
+gx = np.array([[-1, 0, 1],
+               [-2, 0, 2],
+               [-1, 0, 1]])
+
+gy = np.array([[-1, -2, -1],
+               [0, 0, 0],
+               [1, 2, 1]])
+gx_image = blurred_image.filter(ImageFilter.Kernel((3, 3), gx.flatten(), 1))
+gy_image = blurred_image.filter(ImageFilter.Kernel((3, 3), gy.flatten(), 1))
+gx_numpy = np.array(gx_image)
+gy_numpy = np.array(gy_image)
+g_numpy = np.sqrt(gx_numpy.astype(np.uint64) **
+                  2 + gy_numpy.astype(np.uint64)**2)
+theta = np.arctan2(gx_numpy, gy_numpy)
+plt.imshow(gx_numpy)
+plt.axis("off")
+plt.show()
+
+plt.imshow(gy_numpy)
 plt.axis("off")
 plt.show()
--- test.py
+++ test.py
@@ -1,19 +1,26 @@
-import matplotlib.pyplot as plt
-import numpy as np
 from PIL import Image, ImageFilter
+import numpy as np
+import matplotlib.pyplot as plt
 img = Image.open("flower.jpg")
-print(img)
-print(type(img))
-gaussian_kernel = np.array([[1, 2, 1],
-                            [2, 4, 2],
-                            [1, 2, 1]]) / 16.0
-blurred_image = img.filter(
-    ImageFilter.Kernel((3, 3), gaussian_kernel.flatten()))
 
-plt.imshow(img)
+gx = np.array([[-1, 0, 1],
+               [-2, 0, 2],
+               [-1, 0, 1]])
+
+gy = np.array([[-1, -2, -1],
+               [0, 0, 0],
+               [1, 2, 1]])
+gx_image = img.filter(ImageFilter.Kernel((3, 3), gx.flatten(), 1))
+gy_image = img.filter(ImageFilter.Kernel((3, 3), gy.flatten(), 1))
+gx_numpy = np.array(gx_image)
+gy_numpy = np.array(gy_image)
+g_numpy = np.sqrt(gx_numpy.astype(np.uint64) **
+                  2 + gy_numpy.astype(np.uint64)**2)
+theta = np.arctan2(gx_numpy, gy_numpy)
+plt.imshow(gx_numpy)
 plt.axis("off")
 plt.show()
 
-plt.imshow(blurred_image)
+plt.imshow(gy_numpy)
 plt.axis("off")
 plt.show()
